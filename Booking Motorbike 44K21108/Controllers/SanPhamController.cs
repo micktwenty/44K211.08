ef387b33f4ef13f489ff0469b99e7dbb3cec80b4
micktwenty <ngquthien3520@gmail.com>
2021-04-08 14:10:05 +0700

    mit update

---
+++ Booking Motorbike 44K21108/Controllers/SanPhamController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Booking_Motorbike_44K21108.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
