830f0fb0d4e3682f5bd25d7d07a888e00a6b5d29
micktwenty <ngquthien3520@gmail.com>
2021-04-08 15:37:25 +0700

    update

--- Booking Motorbike 44K21108/Controllers/HomeController.cs
+++ Booking Motorbike 44K21108/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Booking_Motorbike_44K21108.Models;
 
 namespace Booking_Motorbike_44K21108.Controllers
 {
     public class HomeController : Controller
     {
+        BMEntities1 db = new BMEntities1();
         // GET: Home
         public ActionResult Index()
         {
+            //tao viewbag
+            var lstXe = db.XEs.Where(n => n.LoaiXe == "Honda");
+            ViewBag.LstXe = lstXe;
+
+            var lstXey = db.XEs.Where(n => n.LoaiXe == "Yamaha");
+            ViewBag.LstXey = lstXey;
+
 
             return View();
         }
