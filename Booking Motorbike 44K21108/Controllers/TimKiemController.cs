846730b01f9bacd194cd4b09fa89c66b9b0fe074
micktwenty <ngquthien3520@gmail.com>
2021-04-28 20:56:23 +0700

    update

---
+++ Booking Motorbike 44K21108/Controllers/TimKiemController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Booking_Motorbike_44K21108.Models;
+using PagedList;
+
+namespace Booking_Motorbike_44K21108.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        BMEntities1 db = new BMEntities1();
+
+        [HttpGet]
+        public ActionResult KQTimKiem(string sTuKhoa, int? page)
+        {
+            if (Request.HttpMethod != "GET")
+            {
+                page = 1;
+            }
+            //Tìm Kiếm the tên sản phẩm
+
+            var lstSP = db.XEs.Where(n => n.TenXe.Contains(sTuKhoa) || n.LoaiXe.Contains(sTuKhoa));
+            int pageSize = 21;
+            int pageNumber = (page ?? 1);
+            return View(lstSP.OrderBy(n => n.GiaXe).ToPagedList(pageNumber, pageSize));
+        }
+
+        [HttpPost]
+        public ActionResult LayTuKhoa(string sTuKhoa)
+        {
+
+            return RedirectToAction("KQTimKiem", new { @sTuKhoa = sTuKhoa });
+        }
+    }
+}
